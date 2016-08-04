cfe4e426064773b14dd068b3361022c5bf55a71b
AndrewZhang1992 <zhanglingyun1992@163.com>
2016-08-04 18:03:05 +0800

    init code

---
+++ LoveApp/LoveApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoveApp
+//
+//  Created by Andrew on 16/8/4.
+//  Copyright © 2016年 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoveApp/LoveApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveApp
+//
+//  Created by Andrew on 16/8/4.
+//  Copyright © 2016年 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
